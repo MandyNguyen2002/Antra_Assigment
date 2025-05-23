3ff886af868734a717cfd8d90f2b39a9d45fc534
Mandy Nguyen <mandynguyen0611@gmail.com>
2025-05-22 21:39:57 -1000

    add test case

--- HW3/Question21.js
+++ HW3/Question21.js
@@ -1,3 +1,10 @@
+/*
+Write a JavaScript function to get all possible subset with a fixed length (for example 2)
+combinations in an array. 
+Sample array: [1, 2, 3] and subset length is 2 
+Expected output: [[2, 1], [3, 1], [3, 2]]
+*/
+
 function getSubset(arr, index) {
   const result = [];
   function combine(start, combo) {
@@ -14,3 +21,5 @@ function getSubset(arr, index) {
   combine(0, []);
   return result;
 }
+
+console.log(getSubset([1, 2, 3], 2));
